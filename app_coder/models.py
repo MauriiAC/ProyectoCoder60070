ebd4f801ba252baeef6bfc5effed66ff8b4aa12b
mauricio.cuello <mauricio.cuello@coderhouse.com>
2024-09-16 22:26:17 -0300

    agregamos nuestros modelos al admin y utilizamos herencia de templates

--- app_coder/models.py
+++ app_coder/models.py
@@ -6,11 +6,23 @@ class Curso(models.Model):
   nombre = models.CharField(max_length=50)
   camada = models.IntegerField()
 
+  def __str__(self):
+    return f'{self.nombre} - {self.camada}'
+  
+  class Meta():
+
+    verbose_name = 'Course'
+    verbose_name_plural = 'My courses...'
+    ordering = ('-nombre', 'camada')
+
 class Estudiante(models.Model):
 
   nombre = models.CharField(max_length=50)
   apellido = models.CharField(max_length=50)
   email = models.EmailField()
+
+  def __str__(self):
+    return f'{self.nombre} {self.apellido}'
 
 class Profesor(models.Model):
 
@@ -24,6 +36,9 @@ class Entregable(models.Model):
   nombre = models.CharField(max_length=50)
   fecha_entrega = models.DateField()
   entregado = models.BooleanField()
+  estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
 
 
+  def __str__(self):
+    return f'{self.nombre}'
 
--- app_coder/urls.py
+++ app_coder/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('agrega-curso/<nombre>/<camada>', crea_curso),
     path('lista-cursos/', lista_cursos),
     path('', inicio),
-    path('profesores/', profesores),
+    path('profesores/', profesores, name='Profesores'),
     path('cursos/', cursos, name="Cursos"),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entregables),
+    path('estudiantes/', estudiantes, name='Estudiantes'),
+    path('entregables/', entregables, name='Entregables'),
 ]
